3a3d1414ae36aca33d2bc209f79f6f237adaf37f
bikram@dey2003 <62688713+modhack2003@users.noreply.github.com>
2024-06-03 17:06:42 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
-import Marquee from 'react-fast-marquee'
 import Marquee_logo from './components/Marquee_logo'
+import Desk from "./assets/desk.jpg"
 import NavBar from './components/NavBar'
 import { Route } from 'react-router-dom'
 import "./App.css"
+import Desk_img from './components/Desk_img'
+
 
 
 const App = () => {
   return (
     <div>
-      <div className='patten'> 
-      <NavBar className="nav"/>
-      <div className='text-container'>
-      <h2>Build and automate custom S&OP workflows</h2>
-      <p>More than just a supply chain planning tool - with the capability to completely transform your work processes. Designed specifically to eliminate the inefficiencies commonly found in traditional methods, resulting in improved accuracy, fill rate, and productivity.</p>
-      <button></button>
+      <div className='patten'>
+        <NavBar className="nav" />
+        <div className='text-container'>
+          <h2>Build and automate custom S&OP workflows</h2>
+          <p>More than just a supply chain planning tool - with the capability to completely transform your work processes. Designed specifically to eliminate the inefficiencies commonly found in traditional methods, resulting in improved accuracy, fill rate, and productivity.</p>
+          <button>Get started with Crest</button>
+          <div className='img-container'><Desk_img/></div>
+        </div>
+
       </div>
-        
-      </div>
-      <Marquee_logo/>
+      <Marquee_logo />
     </div>
   )
 }
---
+++ src/components/Desk_img.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Desk from "../assets/desk.jpg"
+const Desk_img = () => {
+  return (
+   <>
+   <img src={Desk} style={{width:"100%", height:"100%"}}></img>
+   
+   </>
+  )
+}
+
+export default Desk_img
